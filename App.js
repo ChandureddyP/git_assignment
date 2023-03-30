bb22161dd604416b2e8031a2f57251f872466fd1
ChandureddyP <chandusince2002@gmail.com>
2023-03-30 13:05:24 +0530

    final

--- App.js
+++ App.js
@@ -7,11 +7,8 @@ function App() {
       <Navbar />
       <div className="content">
         <Home />
-<<<<<<< HEAD
         <h1>Changes from git-1</h1>
-=======
         <h1>Changes from git-2</h1>
->>>>>>> git_assignment-2/master
       </div>
     </div>
   );
